d1ff1a25a0b0f1ce8179083b8de11177bae6518b
Thomas Haller <thomashaller@gmx.at>
2024-05-22 13:33:02 +0200

    Merge remote-tracking branch WIP'origin/coreValueGuards' into dev

--- contracts/BlockRewardHbbft.sol
+++ contracts/BlockRewardHbbft.sol
@@ -18,7 +18,23 @@ contract BlockRewardHbbft is Initializable, OwnableUpgradeable, ReentrancyGuardU
         uint256 totalRewards;
     }
 
+    /**
+     * @dev Represents a parameter range for a specific getter function.
+     * @param getter The getter function signature.
+     * @param range The range of values for the parameter.
+     */
+    struct ParameterRange {
+        bytes4 getter;
+        uint256[] range;
+    }
+
     // =============================================== Storage ========================================================
+        /**
+     * @dev A mapping that stores the allowed parameter ranges for each function signature.
+     */
+    mapping(bytes4 => ParameterRange) public allowedParameterRange;
+
+    
     mapping(address => uint256[]) internal _epochsPoolGotRewardFor;
 
     /// @dev The reward amount to be distributed in native coins among participants (the validator and their
@@ -108,6 +124,21 @@ contract BlockRewardHbbft is Initializable, OwnableUpgradeable, ReentrancyGuardU
     constructor() {
         // Prevents initialization of implementation contract
         _disableInitializers();
+    }
+
+
+    // ============================================== Modifiers =======================================================
+
+    /**
+     * @dev Modifier to check if a new value is within the allowed range.
+     * @param newVal The new value to be checked.
+     * @notice This modifier is used to ensure that the new value is within the allowed range.
+     * If the new value is not within the allowed range, the function using this modifier
+     * will revert with an error message.
+     */
+    modifier withinAllowedRange(uint256 newVal) {
+        require(isWithinAllowedRange(msg.sig, newVal), "new value not within allowed range");
+        _;
     }
 
     // =============================================== Setters ========================================================
@@ -226,6 +257,48 @@ contract BlockRewardHbbft is Initializable, OwnableUpgradeable, ReentrancyGuardU
         }
 
         // slither-disable-end reentrancy-eth
+    }
+
+    /**
+     * @dev Sets the value of the governancePotShareNominator variable.
+     * @param _shareNominator The new value for the governancePotShareNominator.
+     * Requirements:
+     * - Only the contract owner can call this function.
+     * - The _shareNominator value must be within the allowed range.
+     */
+    function setGovernancePotShareNominator(
+        uint256 _shareNominator
+    ) public onlyOwner withinAllowedRange(_shareNominator) {
+        governancePotShareNominator = _shareNominator;
+    }
+
+    /**
+     * @dev Sets the allowed changeable parameter for a specific setter function.
+     * @param setter The name of the setter function.
+     * @param getter The name of the getter function.
+     * @param params The array of allowed parameter values.
+     * Requirements:
+     * - Only the contract owner can call this function.
+     */
+    function setAllowedChangeableParameter(
+        string memory setter,
+        string memory getter,
+        uint256[] memory params
+    ) external onlyOwner {
+        allowedParameterRange[bytes4(keccak256(bytes(setter)))] = ParameterRange(
+            bytes4(keccak256(bytes(getter))),
+            params
+        );
+    }
+
+    /**
+     * @dev Removes the allowed changeable parameter for a given function selector.
+     * @param funcSelector The function selector for which the allowed changeable parameter should be removed.
+     * Requirements:
+     * - Only the contract owner can call this function.
+     */
+    function removeAllowedChangeableParameter(string memory funcSelector) external onlyOwner {
+        delete allowedParameterRange[bytes4(keccak256(bytes(funcSelector)))];
     }
 
     // =============================================== Getters ========================================================
@@ -462,5 +535,44 @@ contract BlockRewardHbbft is Initializable, OwnableUpgradeable, ReentrancyGuardU
             governancePotShareDenominator;
 
         return shares;
+    }
+
+    /**
+     * @dev Checks if the given `newVal` is within the allowed range for the specified function selector.
+     * @param funcSelector The function selector.
+     * @param newVal The new value to be checked.
+     * @return A boolean indicating whether the `newVal` is within the allowed range.
+     */
+    function isWithinAllowedRange(bytes4 funcSelector, uint256 newVal) public view returns(bool) {
+        ParameterRange memory allowedRange = allowedParameterRange[funcSelector];
+        if(allowedRange.range.length == 0) return false;
+        uint256[] memory range = allowedRange.range;
+        uint256 currVal = _getValueWithSelector(allowedRange.getter);
+        bool currValFound;
+
+        for (uint256 i = 0; i < range.length; i++) {
+            if (range[i] == currVal) {
+                currValFound = true;
+                uint256 leftVal = (i > 0) ? range[i - 1] : range[0];
+                uint256 rightVal = (i < range.length - 1) ? range[i + 1] : range[range.length - 1];
+                if (newVal != leftVal && newVal != rightVal) return false;
+                break;
+            }
+        }
+        return currValFound;
+    }
+
+    // ============================================== Internal ========================================================
+
+    /**
+     * @dev Internal function to get the value of a contract state variable using a getter function.
+     * @param getterSelector The selector of the getter function.
+     * @return The value of the contract state variable.
+     */
+    function _getValueWithSelector(bytes4 getterSelector) private view returns (uint256) {
+        bytes memory payload = abi.encodeWithSelector(getterSelector);
+        (bool success, bytes memory result) = address(this).staticcall(payload);
+        require(success, "Getter call failed");
+        return abi.decode(result, (uint256));
     }
 }
