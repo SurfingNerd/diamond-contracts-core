410f7cd18a76988b1687fd7383a8a6281df4e177
Thomas Haller <thomashaller@gmx.at>
2026-01-21 13:39:33 +0100

    testnet update preparation for V3

--- scripts/upgrades/testnet/generate.sh
+++ scripts/upgrades/testnet/generate.sh
@@ -2,6 +2,6 @@
 
 # todo: figure out what contracts did have changed.
 
-output_file="2025_11_09_contracts.txt"
+output_file="2026_01_20_contracts.txt"
 
-npx hardhat --network testnet getUpgradeCalldata --no-storage-check --output "$output_file" --contract StakingHbbft --init-func initializeV2
+npx hardhat --network testnet getUpgradeCalldata --no-storage-check --output "$output_file" --contract StakingHbbft --init-func initializeV3
